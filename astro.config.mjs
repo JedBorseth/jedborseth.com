c185f2ae4f9db7334fc087015f0372139c19202b
Jed Borseth <jbor@pop-os.localdomain>
2022-10-27 14:46:57 -0700

    fixed astro config error

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 // https://astro.build/config
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
