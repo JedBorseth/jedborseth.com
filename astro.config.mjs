27dd77067540ca4f1bb6d36572b6395c960306d7
Jed Borseth <jbor@pop-os.localdomain>
2022-10-27 15:00:25 -0700

    i hate deploying things

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
-import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
