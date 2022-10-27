11fcbae4b793f2d2ec5ac49e2c423bd94b7ec64b
Jed Borseth <jbor@pop-os.localdomain>
2022-10-27 14:55:01 -0700

    debugging vercel deployment 01

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 // https://astro.build/config
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
