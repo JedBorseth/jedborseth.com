1f3a342374a44103978f2a278ea3e89bba587bea
jedborseth <jedborseth@desktop-popos.va.shawcable.net>
2022-10-31 15:57:32 -0700

    rocket animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,10 @@ module.exports = {
           "0%": { opacity: 0 },
           "100%": { opacity: 1 },
         },
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-47deg)" },
+          "50%": { transform: "rotate(-42deg)" },
+        },
       },
     },
   },
