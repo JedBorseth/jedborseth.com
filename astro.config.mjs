56f8061ac9aa0aa43bca1d0011196132d434f4ba
jedborseth <jedborseth@gmail.com>
2022-11-01 13:56:42 -0700

    turning off ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx(), tailwind()],
-  output: "server",
-  adapter: vercel(),
+  // output: "server",
+  // adapter: vercel(),
 });
