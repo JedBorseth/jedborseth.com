4b94c406e020b0a3fd297c614fc2923199f981ef
jedborseth <jedborseth@desktop-popos.va.shawcable.net>
2022-11-05 14:31:40 -0700

    i might jst not use ssr this is awful

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx(), tailwind()],
-  output: "static",
+  output: "server",
   adapter: vercel(),
 });
